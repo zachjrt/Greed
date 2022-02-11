ec082d72140af4c08a835391c7bb8f862cb89c4c
zachjrt <tho19091@byui.edu>
2022-02-10 18:48:34 -0700

    Window

---
+++ main.py
@@ -0,0 +1,11 @@
+import pyray
+import os
+import random
+
+from shared.color import Color
+from shared.point import Point
+
+pyray.init_window(800, 600, "Greed")
+pyray.set_target_fps(60)
+
+pyray.draw_text('Hello', 400, 300, 12, Color(255, 255, 255))
